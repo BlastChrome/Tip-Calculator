d5cf23bf911bd8e56372b7724a1bff8854371962
nicholas jackson <nicdogg360@gmail.com>
2021-07-21 12:58:36 -0700

    reformatted html

--- tip-calculator/src/App.js
+++ tip-calculator/src/App.js
@@ -2,31 +2,31 @@ import React from "react";
 
 const App = () => {
   return (
-    <div className="container">
-      <h1 className="header-txt-1">SPLI</h1>
-      <h1 className="header-txt-2">TTER</h1>
-      <div className="tip-container">
+    <>
+      <body>
         <div className="container">
-          <div className="top-left-section">
-            <label className="select-txt" htmlFor="bill">
-              Bill
-            </label>
-            <input type="text" name="bill" />
-            <p className="select-txt">Select Tip %</p>
-            <div className="tip-btn-container">
-              <button className="btn">5%</button>
-              <button className="btn">5%</button>
-              <button className="btn">5%</button>
-              <button className="btn">5%</button>
-              <button className="btn">5%</button>
-              <button className="btn">5%</button>
-            </div>
-          </div>
+          <h1 className="header-txt">SPLI</h1>
+          <h1 className="header-txt">TTER</h1>
+          <div className="tip-body">
+            <div className="top-left-section">
+              <label htmlFor="bill">Bill</label>
+              <input type="text" name="bill" />
 
-          <div className="bottom-right-section">Bottom/Right</div>
+              <label htmlFor="Tip">Select Tip %</label>
+              <div className="btn-container">
+                <button className="btn tip-btn">5%</button>
+                <button className="btn tip-btn">10%</button>
+                <button className="btn tip-btn">15%</button>
+                <button className="btn tip-btn">25%</button>
+                <button className="btn tip-btn">50%</button>
+                <button className="btn tip-btn">Custom</button>
+              </div>
+            </div>
+            <div className="bot-right-section">Right Section</div>
+          </div>
         </div>
-      </div>
-    </div>
+      </body>
+    </>
   );
 };
 
